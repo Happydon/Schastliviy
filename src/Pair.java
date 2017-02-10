9c5a7f9c21994e52a8ac8da2f86af416c059c394
unknown <admin@VLAN-25-IKK-538.rd.mirea.ru>
2017-02-10 12:05:20 +0300

    Initial commit

---
+++ src/Pair.java
@@ -0,0 +1,11 @@
+/**
+ * Created by admin on 10.02.2017.
+ */
+public class Pair {
+    String key;
+    String value;
+    Pair(String key,String Value){
+        this.key=key;
+        this.value=value;
+    }
+}
